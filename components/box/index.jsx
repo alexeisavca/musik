6bf8e8cf8e2f639336335d666834c67aeeb746cd
Alexei <savca.alexei@gmail.com>
2015-06-22 00:41:14 +0300

    Moved main navigation box's filter field to the protoclass

--- components/box/index.jsx
+++ components/box/index.jsx
@@ -1,9 +1,13 @@
 var React = require('react');
+var __ = require('../../tools/translate');
 require('./style');
 module.exports = class extends React.Component {
     wrap (children){
         return (
             <div className="col-md-2 col-sm-5 musik-main-navigation-box">
+                <form>
+                    <input type="text" className="form-control" placeholder={__('Search')}/>
+                </form>
                 {children}
             </div>
         );
--- components/box/search.jsx
+++ components/box/search.jsx
@@ -2,11 +2,9 @@ var React = require('react');
 var Box = require('./index');
 var __ = require('../../tools/translate');
 module.exports = class extends Box {
-    render() {
+    render () {
         return this.wrap(
-            <form>
-                <input type="text" className="form-control" placeholder={__('Search')}/>
-            </form>
+            "here be search"
         )
     }
 };
