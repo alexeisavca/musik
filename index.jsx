3bf6feda393a30598ae5145a4dca42838205c3d5
Alexei <savca.alexei@gmail.com>
2015-06-21 23:10:39 +0300

    Delegating .rows to main navigation and main navigation boxes. Added logic for choosing the main navigation box

--- index.jsx
+++ index.jsx
@@ -2,18 +2,26 @@ var React = require('react');
 var __ = require('./tools/translate');
 var MainNavigation = require("./components/main-navigation");
 var Playlist = require('./components/playlist');
-var Library = require('./components/placeholder')('library');
+var SearchBox = require('./components/box/search');
+var AddLibraryBox = require('./components/placeholder')('add library box');
+var LibraryBox = require('./components/placeholder')('library box');
 module.exports = Musik;
 class Musik extends React.Component{
+    getMainNavigationBox(){
+        if(this.props.libraries.some(library => library.get('slug') == this.props.currentMainNavigationItem)){
+            return <LibraryBox/>;
+        } else if('add-library' == this.props.currentMainNavigationItem){
+            return <AddLibraryBox/>;
+        } else {
+            return <SearchBox/>
+        }
+    }
+
     render(){
         return (
             <div className="row">
-                <div className="col-md-1 col-sm-1">
-                    <MainNavigation libraries={this.props.libraries} currentItem={this.props.currentMainNavigationItem}/>
-                </div>
-                <div className="col-md-2 col-sm-5">
-                    <Library/>
-                </div>
+                <MainNavigation libraries={this.props.libraries} currentItem={this.props.currentMainNavigationItem}/>
+                {this.getMainNavigationBox()}
                 <div className="col-md-9 col-sm-6">
                     <div className="row">
                         <div className="col-md-12">
