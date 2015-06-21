5dcdabdca73cb88018daf5afdae96db543696456
Alexei <savca.alexei@gmail.com>
2015-06-21 23:05:24 +0300

    Turns out I don't actually need an inline form

--- components/box/search.jsx
+++ components/box/search.jsx
@@ -4,10 +4,8 @@ var __ = require('../../tools/translate');
 module.exports = class extends Box {
     render() {
         return this.wrap(
-            <form className="form-inline">
-                <div className="form-group">
-                    <input type="text" className="form-control" placeholder={__('Search')}/>
-                </div>
+            <form>
+                <input type="text" className="form-control" placeholder={__('Search')}/>
             </form>
         )
     }
