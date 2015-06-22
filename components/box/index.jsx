23c3e4e17bb3953d6e0bcfd43f5fed6026fa41f0
Alexei <savca.alexei@gmail.com>
2015-06-22 03:22:47 +0300

    Boxes now have a filter prop

--- components/box/index.jsx
+++ components/box/index.jsx
@@ -1,15 +1,26 @@
 var React = require('react');
 var __ = require('../../tools/translate');
+var {Map} = require('immutable');
 require('./style');
-module.exports = class extends React.Component {
+module.exports = Box;
+class Box extends React.Component {
     wrap (children){
         return (
             <div className="col-md-2 col-sm-5 musik-main-navigation-box">
                 <form>
-                    <input type="text" className="form-control" placeholder={__('Search')}/>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder={__('Search')}
+                        value={this.props.filter}
+                    />
                 </form>
                 {children}
             </div>
         );
     }
+};
+Box.propTypes = {
+    filter: React.PropTypes.string,
+    library: React.PropTypes.instanceOf(Map).isRequired
 };
