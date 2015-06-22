ef2d2ef0c5c6a5b16cd0d34486a0d3262c93f04b
Alexei <savca.alexei@gmail.com>
2015-06-22 03:35:53 +0300

    Filter form is now returned by an overridable function

--- components/box/index.jsx
+++ components/box/index.jsx
@@ -4,22 +4,28 @@ var {Map} = require('immutable');
 require('./style');
 module.exports = Box;
 class Box extends React.Component {
+    getFilterForm (){
+        return (
+            <form>
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder={__('Search')}
+                    value={this.props.filter}
+                    />
+            </form>
+        )
+    }
+
     wrap (children){
         return (
             <div className="col-md-2 col-sm-5 musik-main-navigation-box">
-                <form>
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder={__('Search')}
-                        value={this.props.filter}
-                    />
-                </form>
+                {this.getFilterForm()}
                 {children}
             </div>
         );
     }
-};
+}
 Box.propTypes = {
     filter: React.PropTypes.string,
     library: React.PropTypes.instanceOf(Map).isRequired
