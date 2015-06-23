b28bc4b41db1bd3f53946e3526ad4ffc3366ca74
Alexei <savca.alexei@gmail.com>
2015-06-23 18:43:57 +0300

    Refactored navigation box. Cleaned up the proto class, all fitering functionality moved to LibraryBox

--- components/box/index.jsx
+++ components/box/index.jsx
@@ -1,38 +1,14 @@
 var React = require('react');
 var __ = require('../../tools/translate');
-var {Map} = require('immutable');
+var obj2arr = require('../../tools/obj2arr');
 require('./style');
 module.exports = Box;
 class Box extends React.Component {
-    updateBoxFilter (e){
-        this.props.setBoxFilter(e.target.value);
-    }
-
-    getFilterForm (){
-        return (
-            <form>
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder={__('Search')}
-                    value={this.props.filter}
-                    onChange={this.updateBoxFilter.bind(this)}
-                />
-            </form>
-        )
-    }
-
-    wrap (children){
+    wrap (){
         return (
             <div className="col-md-2 col-sm-5 musik-main-navigation-box">
-                {this.getFilterForm()}
-                {children}
+                {obj2arr(arguments)}
             </div>
         );
     }
 }
-Box.propTypes = {
-    filter: React.PropTypes.string,
-    setBoxFilter: React.PropTypes.func.isRequired,
-    library: React.PropTypes.instanceOf(Map).isRequired
-};
--- components/box/library.jsx
+++ components/box/library.jsx
@@ -1,10 +1,36 @@
 var React = require('react');
 var Box = require('./index');
 var __ = require('../../tools/translate');
-module.exports = class extends Box {
+module.exports = LibraryBox;
+class LibraryBox extends Box {
+    updateBoxFilter (e){
+        this.props.setBoxFilter(e.target.value);
+    }
+
+    getFilterForm (){
+        return (
+            <form>
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder={__('Search')}
+                    value={this.props.filter}
+                    onChange={this.updateBoxFilter.bind(this)}
+                    />
+            </form>
+        )
+    }
+
     render () {
         return this.wrap(
+            this.getFilterForm(),
             this.props.library.get('tracks').map(track => track.get('title'))
         )
     }
+}
+
+LibraryBox.propTypes = {
+    filter: React.PropTypes.string,
+    setBoxFilter: React.PropTypes.func.isRequired,
+    library: React.PropTypes.instanceOf(Map).isRequired
 };
