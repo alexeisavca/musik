0a0e6ca7d6151db3dabd2c5bbb5c8f20b0fd281e
Alexei <savca.alexei@gmail.com>
2015-06-23 18:45:55 +0300

    Box protoclass is now a pure render component

--- components/box/index.jsx
+++ components/box/index.jsx
@@ -1,9 +1,10 @@
 var React = require('react');
 var __ = require('../../tools/translate');
 var obj2arr = require('../../tools/obj2arr');
+var PureRenderComponent = require('../pure-render-component');
 require('./style');
 module.exports = Box;
-class Box extends React.Component {
+class Box extends PureRenderComponent {
     wrap (){
         return (
             <div className="col-md-2 col-sm-5 musik-main-navigation-box">
