04c9e8cd6ce86d96746c14e771de6a41122348f2
Alexei <savca.alexei@gmail.com>
2015-06-24 05:37:52 +0300

    Updated PureRenderComponent so it supports state now

--- components/pure-render-component.jsx
+++ components/pure-render-component.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
+var shallowDiff = (a,b) => Object.keys(a).some(key => a[key] != b[key]);
 module.exports = class extends React.Component {
-    shouldComponentUpdate (nextProps){
-        return Object.keys(this.props).some(key => this.props[key] != nextProps[key])
+    shouldComponentUpdate (nextProps, nextState){
+        return shallowDiff(this.props, nextProps) || shallowDiff(this.state, nextState);
     }
 };
