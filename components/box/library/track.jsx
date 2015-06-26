9e8513e6d9d7ff9bb5f29d74a138f541ad86d4a1
Alexei <savca.alexei@gmail.com>
2015-06-26 08:58:41 +0300

    Allowing dragging for individual tracks

--- components/box/library/track.jsx
+++ components/box/library/track.jsx
@@ -1,11 +1,17 @@
 var React = require('react');
 var PureRenderComponent = require('../../pure-render-component');
+var {Map} = require('immutable');
 module.exports = Track;
 class Track extends PureRenderComponent {
+    onDragStart (e){
+        e.dataTransfer.effectAllowed = 'copy';
+        e.dataTransfer.setData('text/plain', JSON.stringify([this.props.track]));
+    }
+
     render() {
         var {name} = this.props;
         return (
-            <div className="media">
+            <div className="media" draggable onDragStart={this.onDragStart.bind(this)}>
                 <div className="media-left">
                     <a href="javascript:void(0)">
                         <img className="media-object" src="https://placeholdit.imgix.net/~text?txtsize=9&txt=32%C3%9732&w=32&h=32" alt={name}/>
@@ -19,5 +25,6 @@ class Track extends PureRenderComponent {
     }
 }
 Track.propTypes = {
-    name: React.PropTypes.string.isRequired
+    name: React.PropTypes.string.isRequired,
+    track: React.PropTypes.instanceOf(Map).isRequired
 };
