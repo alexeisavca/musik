5fc0c924c6162b71f666f9e8d1c39ff5492d2c34
Alexei <savca.alexei@gmail.com>
2015-06-26 09:38:35 +0300

    Destubbed <Playlist/> Track iteration and dropping now works, added prop types

--- components/playlist/index.jsx
+++ components/playlist/index.jsx
@@ -1,25 +1,42 @@
 var React = require('react');
-module.exports = class extends React.Component {
-    render() {
+var PureRenderComponent = require('../pure-render-component');
+var __ = require('../../tools/translate');
+var Immutable = require('immutable');
+var {List} = Immutable;
+var Track = require('./track');
+module.exports = Playlist;
+class Playlist extends PureRenderComponent {
+    onDragOver (e){
+        e.preventDefault();
+    }
+
+    onDrop (e){
+        e.preventDefault();
+        this.props.updatePlaylistTracks(Immutable.fromJS(JSON.parse(e.dataTransfer.getData('text'))));
+    }
+
+    render () {
         return (
-            <table className="table">
+            <table className="table" onDrop={this.onDrop.bind(this)} onDragOver={this.onDragOver.bind(this)}>
                 <thead>
                     <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
+                        <th>{__('Track')}</th>
+                        <th>{__('Title')}</th>
+                        <th>{__('Artist')}</th>
+                        <th>{__('Album')}</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
+                    {this.props.tracks.map((track, index) => (
+                        <Track key={index} track={track} setCurrentTrack={this.props.setCurrentTrack}/>
+                    ))}
                 </tbody>
             </table>
         )
     }
+}
+Playlist.propTypes = {
+    tracks: React.PropTypes.instanceOf(List).isRequired,
+    updatePlaylistTracks: React.PropTypes.func.isRequired,
+    setCurrentTrack: React.PropTypes.func.isRequired
 };
