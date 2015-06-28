44c5dc35bcaad44feab14961b71164c9b6e3fa4f
Alexei <savca.alexei@gmail.com>
2015-06-28 19:18:44 +0300

    Changes for <Player/>: placeholders for seekbar and equalizer hid the <audio> tag instaplaying

--- components/player/index.jsx
+++ components/player/index.jsx
@@ -1,11 +1,51 @@
 var React = require('react');
 var {Map} = require('immutable');
+var PureRenderComponent = require('../pure-render-component');
+var Seekbar = require('../placeholder')("seek bar");
+var Equalizer = require('../placeholder')("equalizer");
 module.exports = Player;
-class Player extends React.Component {
+class Player extends PureRenderComponent {
+    getTheAudioTag (){
+        return this.refs.theTag.getDOMNode();
+    }
+    componentDidMount (){
+        this.getTheAudioTag().play();
+    }
+
     render() {
         var {track} = this.props;
         return (
-            <audio src={track && track.get('url')} controls autoplay/>
+            <div className="col-md-12">
+                <audio src={track && track.get('url')} style={{display: 'none'}} ref="theTag"/>
+                <div className="col-sm-5 col-md-3">
+                    <div className="col-sm-3">
+                        <button className="btn btn-block btn-default">
+                            <i className="glyphicon glyphicon-backward"></i>
+                        </button>
+                    </div>
+                    <div className="col-sm-3">
+                        <button className="btn btn-block btn-default">
+                            <i className="glyphicon glyphicon-play"></i>
+                        </button>
+                    </div>
+                    <div className="col-sm-3">
+                        <button className="btn btn-block btn-default">
+                            <i className="glyphicon glyphicon-stop"></i>
+                        </button>
+                    </div>
+                    <div className="col-sm-3">
+                        <button className="btn btn-block btn-default">
+                            <i className="glyphicon glyphicon-forward"></i>
+                        </button>
+                    </div>
+                </div>
+                <div className="col-md-9 col-sm-7">
+                    <Equalizer/>
+                </div>
+                <div className="col-md-12">
+                    <Seekbar/>
+                </div>
+            </div>
         )
     }
 }
